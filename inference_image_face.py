ccbf5ee31e217e0f5f4fb54c9afff937d94cbdb5
benckx <benoit.vleminckx@gmail.com>
2019-03-01 12:58:30 +0100

    Quick and dirty implementation / First commit

---
+++ inference_image_face.py
@@ -0,0 +1,94 @@
+import io
+import sys
+import time
+from os import listdir
+from os.path import isfile, join
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+from utils import label_map_util
+from utils import visualization_utils_color as vis_util
+
+sys.path.append("..")
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = './model/frozen_inference_graph_face.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = './protos/face_label_map.pbtxt'
+
+NUM_CLASSES = 2
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+image_folder = 'test_images'
+image_names = [f for f in listdir(image_folder) if (isfile(join(image_folder, f)) and (f.endswith(".jpg") or f.endswith(".jpeg")))]
+
+output = open("result.csv", "w+")
+
+with detection_graph.as_default():
+  config = tf.ConfigProto()
+  config.gpu_options.allow_growth = True
+  with tf.Session(graph=detection_graph, config=config) as sess:
+    for image_name in image_names:
+      image_path = image_folder + '/' + image_name
+      print(image_path)
+      image_data = open(image_path, "rb").read()
+      image = Image.open(io.BytesIO(image_data))
+      rgb_im = image.convert('RGB')
+      width = rgb_im.size[0]
+      height = rgb_im.size[1]
+
+      image_np_expanded = np.expand_dims(image, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+      # Actual detection.
+      start_time = time.time()
+      (boxes, scores, classes, num_detections) = sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+      elapsed_time = time.time() - start_time
+      print('inference time cost: {}'.format(elapsed_time))
+
+      boxes_to_draw = vis_util.get_boxes(
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index)
+
+      scores_readables = vis_util.get_scores(
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index)
+
+      print("score: " + str(scores_readables))
+
+      i = 0
+      for box, color in boxes_to_draw:
+        print()
+        ymin, xmin, ymax, xmax = box
+        x1 = int(xmin * width)
+        y1 = int(ymin * height)
+        x2 = int(xmax * width)
+        y2 = int(ymax * height)
+        row = '{},{},{},{},{},{}'.format(image_path, scores_readables[i], x1, y2, x2, y2)
+        output.write(row + "\n")
+        i += 1
--- utils/visualization_utils_color.py
+++ utils/visualization_utils_color.py
@@ -20,41 +20,41 @@ The functions do not return a value, instead they modify the image itself.
 
 """
 import collections
-import numpy as np
+
 import PIL.Image as Image
 import PIL.ImageColor as ImageColor
 import PIL.ImageDraw as ImageDraw
 import PIL.ImageFont as ImageFont
+import numpy as np
 import six
 import tensorflow as tf
-
 
 _TITLE_LEFT_MARGIN = 10
 _TITLE_TOP_MARGIN = 10
 STANDARD_COLORS = [
-    'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
-    'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
-    'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
-    'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
-    'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
-    'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
-    'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
-    'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
-    'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',
-    'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
-    'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
-    'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
-    'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
-    'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
-    'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
-    'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
-    'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
-    'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
-    'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
-    'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
-    'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
-    'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
-    'WhiteSmoke', 'Yellow', 'YellowGreen'
+  'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
+  'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
+  'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
+  'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
+  'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
+  'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
+  'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+  'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
+  'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',
+  'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
+  'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
+  'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
+  'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
+  'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
+  'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
+  'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+  'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
+  'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
+  'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
+  'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+  'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
+  'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
+  'WhiteSmoke', 'Yellow', 'YellowGreen'
 ]
 
 
@@ -167,14 +167,14 @@ def draw_bounding_box_on_image(image,
     text_width, text_height = font.getsize(display_str)
     margin = np.ceil(0.05 * text_height)
     draw.rectangle(
-        [(left, text_bottom - text_height - 2 * margin), (left + text_width,
-                                                          text_bottom)],
-        fill=color)
+      [(left, text_bottom - text_height - 2 * margin), (left + text_width,
+                                                        text_bottom)],
+      fill=color)
     draw.text(
-        (left + margin, text_bottom - text_height - margin),
-        display_str,
-        fill='black',
-        font=font)
+      (left + margin, text_bottom - text_height - margin),
+      display_str,
+      fill='black',
+      font=font)
     text_bottom -= text_height - 2 * margin
 
 
@@ -313,9 +313,9 @@ def draw_mask_on_image_array(image, mask, color='red', alpha=0.7):
   pil_image = Image.fromarray(image)
 
   solid_color = np.expand_dims(
-      np.ones_like(mask), axis=2) * np.reshape(list(rgb), [1, 1, 3])
+    np.ones_like(mask), axis=2) * np.reshape(list(rgb), [1, 1, 3])
   pil_solid_color = Image.fromarray(np.uint8(solid_color)).convert('RGBA')
-  pil_mask = Image.fromarray(np.uint8(255.0*alpha*mask)).convert('L')
+  pil_mask = Image.fromarray(np.uint8(255.0 * alpha * mask)).convert('L')
   pil_image = Image.composite(pil_solid_color, pil_image, pil_mask)
   np.copyto(image, np.array(pil_image.convert('RGB')))
 
@@ -386,8 +386,8 @@ def visualize_boxes_and_labels_on_image_array(image,
           else:
             class_name = 'N/A'
           display_str = '{}: {}%'.format(
-              class_name,
-              int(100*scores[i]))
+            class_name,
+            int(100 * scores[i]))
         else:
           display_str = 'score: {}%'.format(int(100 * scores[i]))
         box_to_display_str_map[box].append(display_str)
@@ -395,7 +395,7 @@ def visualize_boxes_and_labels_on_image_array(image,
           box_to_color_map[box] = 'DarkOrange'
         else:
           box_to_color_map[box] = STANDARD_COLORS[
-              classes[i] % len(STANDARD_COLORS)]
+            classes[i] % len(STANDARD_COLORS)]
 
   # Draw all boxes onto image.
   for box, color in box_to_color_map.items():
@@ -403,24 +403,115 @@ def visualize_boxes_and_labels_on_image_array(image,
     ymin, xmin, ymax, xmax = box
     if instance_masks is not None:
       draw_mask_on_image_array(
-          image,
-          box_to_instance_masks_map[box],
-          color=color
+        image,
+        box_to_instance_masks_map[box],
+        color=color
       )
     draw_bounding_box_on_image_array(
-        image,
-        ymin,
-        xmin,
-        ymax,
-        xmax,
-        color=color,
-        thickness=line_thickness,
-        display_str_list=box_to_display_str_map[box],
-        use_normalized_coordinates=use_normalized_coordinates)
+      image,
+      ymin,
+      xmin,
+      ymax,
+      xmax,
+      color=color,
+      thickness=line_thickness,
+      display_str_list=box_to_display_str_map[box],
+      use_normalized_coordinates=use_normalized_coordinates)
     if keypoints is not None:
       draw_keypoints_on_image_array(
-          image,
-          box_to_keypoints_map[box],
-          color=color,
-          radius=line_thickness / 2,
-          use_normalized_coordinates=use_normalized_coordinates)
+        image,
+        box_to_keypoints_map[box],
+        color=color,
+        radius=line_thickness / 2,
+        use_normalized_coordinates=use_normalized_coordinates)
+
+
+def get_boxes(boxes,
+              classes,
+              scores,
+              category_index,
+              instance_masks=None,
+              keypoints=None,
+              max_boxes_to_draw=20,
+              min_score_thresh=.7,
+              agnostic_mode=False):
+  box_to_display_str_map = collections.defaultdict(list)
+  box_to_color_map = collections.defaultdict(str)
+  box_to_instance_masks_map = {}
+  box_to_keypoints_map = collections.defaultdict(list)
+  if not max_boxes_to_draw:
+    max_boxes_to_draw = boxes.shape[0]
+  for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+    if scores is None or scores[i] > min_score_thresh:
+      box = tuple(boxes[i].tolist())
+      if instance_masks is not None:
+        box_to_instance_masks_map[box] = instance_masks[i]
+      if keypoints is not None:
+        box_to_keypoints_map[box].extend(keypoints[i])
+      if scores is None:
+        box_to_color_map[box] = 'black'
+      else:
+        if not agnostic_mode:
+          if classes[i] in category_index.keys():
+            class_name = category_index[classes[i]]['name']
+          else:
+            class_name = 'N/A'
+          display_str = '{}: {}%'.format(
+            class_name,
+            int(100 * scores[i]))
+        else:
+          display_str = 'score: {}%'.format(int(100 * scores[i]))
+        box_to_display_str_map[box].append(display_str)
+        if agnostic_mode:
+          box_to_color_map[box] = 'DarkOrange'
+        else:
+          box_to_color_map[box] = STANDARD_COLORS[
+            classes[i] % len(STANDARD_COLORS)]
+
+  return box_to_color_map.items()
+
+def get_scores(boxes,
+              classes,
+              scores,
+              category_index,
+              instance_masks=None,
+              keypoints=None,
+              max_boxes_to_draw=20,
+              min_score_thresh=.7,
+              agnostic_mode=False):
+  result = []
+  box_to_display_str_map = collections.defaultdict(list)
+  box_to_color_map = collections.defaultdict(str)
+  box_to_instance_masks_map = {}
+  box_to_keypoints_map = collections.defaultdict(list)
+  if not max_boxes_to_draw:
+    max_boxes_to_draw = boxes.shape[0]
+  for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+    if scores is None or scores[i] > min_score_thresh:
+      box = tuple(boxes[i].tolist())
+      if instance_masks is not None:
+        box_to_instance_masks_map[box] = instance_masks[i]
+      if keypoints is not None:
+        box_to_keypoints_map[box].extend(keypoints[i])
+      if scores is None:
+        box_to_color_map[box] = 'black'
+      else:
+        if not agnostic_mode:
+          if classes[i] in category_index.keys():
+            class_name = category_index[classes[i]]['name']
+          else:
+            class_name = 'N/A'
+          display_str = '{}: {}%'.format(
+            class_name,
+            int(100 * scores[i]))
+          result.append(scores[i])
+        else:
+          display_str = 'score: {}%'.format(int(100 * scores[i]))
+        box_to_display_str_map[box].append(display_str)
+        if agnostic_mode:
+          box_to_color_map[box] = 'DarkOrange'
+        else:
+          box_to_color_map[box] = STANDARD_COLORS[
+            classes[i] % len(STANDARD_COLORS)]
+
+  return result
